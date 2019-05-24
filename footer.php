c7509477fcd481f07592b5f3818fc677d0383d2f
rubeena tamang <rubeenatamang9@gmail.com>
2019-05-24 22:01:46 +0545

    foot update

--- footer.php
+++ footer.php
@@ -26,42 +26,58 @@
 				<div class="row">
 					<div class="col-lg-3  col-md-6 col-sm-6">
 						<div class="single-footer-widget">
-							<h4 class="text-white">Medicine Us</h4>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore
-								magna aliqua.
+							<h4 class="text-white">About WebDoc</h4>
+							<p><ul><li>
+								Find nearby Hospitals</li>
+<li>Know about doctors in their specialized fields</li><li>
+Booking tickets and taking appointments</li><li>
+Get emergency calls.</li></ul>
+
 							</p>
 						</div>
 					</div>
 					<div class="col-lg-4  col-md-6 col-sm-6">
 						<div class="single-footer-widget">
-							<h4 class="text-white">Contact Us</h4>
+							<h4 class="text-white">Our Targets</h4>
 							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore
-								magna aliqua.
-							</p>
+								Link up Patients,Doctors and Hospitals.
+							</p><h4 class="text-white">Contact Us</h4>
 							<p class="number">
-								012-6532-568-9746 <br>
-								012-6532-569-9748
+								9812345678 <br>
+								9851234567
 							</p>
 						</div>
 					</div>
 					<div class="col-lg-5  col-md-6 col-sm-6">
 						<div class="single-footer-widget">
-							<h4 class="text-white">Newsletter</h4>
-							<p>You can trust us. we only send offers, not a single spam.</p>
+							<h4 class="text-white">Comment/Feedback</h4>
+							
 							<div class="d-flex flex-row" id="mc_embed_signup">
 								<form class="navbar-form" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
 								 method="get">
 									<div class="input-group add-on">
 										<input class="form-control" name="EMAIL" placeholder="Email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email address'"
 										 required="" type="email">
+
 										<div style="position: absolute; left: -5000px;">
 											<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
 										</div>
 										<div class="input-group-btn">
 											<button class="genric-btn text-uppercase">
 												get started
+											</button>
+										</div>
+									</div>
+									<div class="input-group add-on">
+										<input class="form-control" name="EMAIL" placeholder="Comment/Feedback" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Comment/Feedback'"
+										 required="" type="textbox">
+										 
+										<div style="position: absolute; left: -5000px;">
+											<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
+										</div>
+										<div class="input-group-btn">
+											<button class="genric-btn text-uppercase">
+												Send
 											</button>
 										</div>
 									</div>
@@ -78,14 +94,12 @@
 		<div class="footer-bottom">
 			<div class="container">
 				<div class="d-flex justify-content-between align-items-center flex-wrap">
-					<p class="footer-text"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+					
 					<div class="footer-social d-flex align-items-center">
-						<a href="#"><i class="fa fa-facebook"></i></a>
-						<a href="#"><i class="fa fa-twitter"></i></a>
-						<a href="#"><i class="fa fa-dribbble"></i></a>
-						<a href="#"><i class="fa fa-behance"></i></a>
+						<a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a>
+						<a href="https://twitter.com"><i class="fa fa-twitter"></i></a>
+						<a href="https://dribbble.com/"><i class="fa fa-dribbble"></i></a>
+						<a href="https://www.behance.net/"><i class="fa fa-behance"></i></a>
 					</div>
 				</div>
 			</div>
