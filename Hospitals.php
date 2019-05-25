085d372d952b8a268d0954fb9432bdf7debf54d4
rubeena tamang <rubeenatamang9@gmail.com>
2019-05-25 08:04:01 +0545

    Changed

--- Hospitals.php
+++ Hospitals.php
@@ -22,32 +22,21 @@ include 'header.php';
 		</p>
 		<a class="primary-btn text-uppercase" href="login.php">Get Appointment</a>
 	</div>
+</div>
+<div class="hosp_body">
+	
 	<div class="hosp_img">
 		<img  id="hosp"src="img/hospitals/grande.jpg">
 	</div>
 	<div class="hosp_name">
-		<a href="">Manmohan Memorial Community Hospital</a>
+		<a href="">GRANDE INTERNATIONAL HOSPITAL</a>
 		<p>
-			Thamel, Kathmandu (Opposite Sarswati Campus) Kathmandu-32, Nepal 
-			 977-1-4411587, 4411605, 4420822, 4420823
+			Dhapasi, Kathmandu, Nepal
+			01-5159266, 01-5159267, 9801202550
 			
 		</p>
 		<a class="primary-btn text-uppercase" href="login.php">Get Appointment</a>
 	</div>
-	<div class="hosp_img">
-		<img  id="hosp"src="img/hospitals/grande.jpg">
-	</div>
-	<div class="hosp_name">
-		<a href="">Manmohan Memorial Community Hospital</a>
-		<p>
-			Thamel, Kathmandu (Opposite Sarswati Campus) Kathmandu-32, Nepal 
-			 977-1-4411587, 4411605, 4420822, 4420823
-			
-		</p>
-		<a class="primary-btn text-uppercase" href="login.php">Get Appointment</a>
-	</div>
-
-
 </div>
 	
 </body>
