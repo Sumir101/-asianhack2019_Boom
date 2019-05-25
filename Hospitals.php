a9540cfaaf9db3e268790e5f737b470e361032c3
rubeena tamang <rubeenatamang9@gmail.com>
2019-05-25 09:19:03 +0545

    hospital list complete

--- Hospitals.php
+++ Hospitals.php
@@ -8,13 +8,30 @@ include 'header.php';
 	<link rel="stylesheet" type="text/css" href="css/hospital.css">
 </head>
 <body>
+	<hr>
+	<div class="hosp_body">
+	
+	<div class="hosp_img">
+		<img  id="hosp"src="img/hospitals/vinayak.jpg">
+	</div>
+	<div class="hosp_name">
+		<a href="http://vinayakhospital.com.np/">Vinayak Memorial Hospital</a>
+		<p>
+			Gongabu, Kathmandu, Nepal
+			5159272
+			
+		</p>
+		<a class="primary-btn text-uppercase" href="login.php">Get Appointment</a>
+	</div>
+</div>
+<hr>
 <div class="hosp_body">
 	
 	<div class="hosp_img">
 		<img  id="hosp"src="img/hospitals/grande.jpg">
 	</div>
 	<div class="hosp_name">
-		<a href="">GRANDE INTERNATIONAL HOSPITAL</a>
+		<a href="https://www.grandehospital.com/">GRANDE INTERNATIONAL HOSPITAL</a>
 		<p>
 			Dhapasi, Kathmandu, Nepal
 			01-5159266, 01-5159267, 9801202550
@@ -23,6 +40,8 @@ include 'header.php';
 		<a class="primary-btn text-uppercase" href="login.php">Get Appointment</a>
 	</div>
 </div>
+<hr>
+
 
 	<div class="hosp_body">
 	
@@ -30,7 +49,7 @@ include 'header.php';
 		<img  id="hosp"src="img/hospitals/ishan.jpg">
 	</div>
 	<div class="hosp_name">
-		<a href="">ISHAN CHILDREN & WOMAN'S HOSPITAL (P).LTD.</a>
+		<a href="https://ishanhospital.000webhostapp.com/">ISHAN CHILDREN & WOMAN'S HOSPITAL (P).LTD.</a>
 		<p>
 			Basundhara, Kathmandu, Nepal
 			01-4354537,01-4381962
@@ -39,22 +58,69 @@ include 'header.php';
 	</div>
 
 </div>
+<hr>
 <div class="hosp_body">
 	
 	<div class="hosp_img">
 		<img  id="hosp"src="img/hospitals/manmohan.jpg">
 	</div>
 	<div class="hosp_name">
-		<a href="">MANMOHAN CARDIOTHORACIC VASCULAR AND TRANSPLANT CENTER (MCVTC)</a>
+		<a href="http://mmth.org.np/">MANMOHAN CARDIOTHORACIC VASCULAR AND TRANSPLANT CENTER (MCVTC)</a>
 		<p>
 			TUTH Complex, Maharajgunj, Kathmandu, Nepal
 			01-4418822, 01-4418833
 		</p>
 		<a class="primary-btn text-uppercase" href="login.php">Get Appointment</a>
 	</div>
-
 </div>
+<hr>
+<div class="hosp_body">
 	
+	<div class="hosp_img">
+		<img  id="hosp"src="img/hospitals/Green City.jpg">
+	</div>
+	<div class="hosp_name">
+		<a href="https://www.gch.com.np/">Green City Hospital</a>
+		<p>
+			Basundhara, Kahtmandu, Nepal
+			+977-1-4381133, 4383169, 4352248
+		</p>
+		<a class="primary-btn text-uppercase" href="login.php">Get Appointment</a>
+	</div>
+</div>
+<hr>
+
+<div class="hosp_body">
+	
+	<div class="hosp_img">
+		<img  id="hosp"src="img/hospitals/norvic.jpg">
+	</div>
+	<div class="hosp_name">
+		<a href="https://norvichospital.com/">Norvic Hospital </a>
+		<p>
+			Thapathali,Kathmandu, Nepal
+			+977-1-4258554 
++977-1-4218230
+		</p>
+		<a class="primary-btn text-uppercase" href="login.php">Get Appointment</a>
+	</div>
+</div>
+<hr>
+	<div class="hosp_body">
+	
+	<div class="hosp_img">
+		<img  id="hosp"src="img/hospitals/bir.jpg">
+	</div>
+	<div class="hosp_name">
+		<a href="https://nams.org.np/contact.php">Bir Hospital </a>
+		<p>
+			Mahaboudha,Kathmandu, Nepal
+			+977-1-4221119, 4221988, 4222865
+		</p>
+		<a class="primary-btn text-uppercase" href="login.php">Get Appointment</a>
+	</div>
+</div>
+<hr>
 </body>
 </html>
 <?php
