23100f5d929430b58e95a9feab8b4e005817386d
Sumir101 <sumiracharya101@gmail.com>
2019-05-25 08:01:12 +0545

    Hospital page updated

--- Hospitals.php
+++ Hospitals.php
@@ -24,6 +24,36 @@ include 'header.php';
 	</div>
 
 </div>
+	<div class="hosp_body">
+	
+	<div class="hosp_img">
+		<img  id="hosp"src="img/hospitals/grande.jpg">
+	</div>
+	<div class="hosp_name">
+		<a href="">ISHAN CHILDREN & WOMAN'S HOSPITAL (P).LTD.</a>
+		<p>
+			Basundhara, Kathmandu, Nepal
+			01-4354537,01-4381962
+		</p>
+		<a class="primary-btn text-uppercase" href="login.php">Get Appointment</a>
+	</div>
+
+</div>
+<div class="hosp_body">
+	
+	<div class="hosp_img">
+		<img  id="hosp"src="img/hospitals/grande.jpg">
+	</div>
+	<div class="hosp_name">
+		<a href="">MANMOHAN CARDIOTHORACIC VASCULAR AND TRANSPLANT CENTER (MCVTC)</a>
+		<p>
+			TUTH Complex, Maharajgunj, Kathmandu, Nepal
+			01-4418822, 01-4418833
+		</p>
+		<a class="primary-btn text-uppercase" href="login.php">Get Appointment</a>
+	</div>
+
+</div>
 	
 </body>
 </html>
